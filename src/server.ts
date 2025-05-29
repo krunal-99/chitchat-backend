22dbe4cec043861a9ca5cb99a72d84c6a7f88328
Krunal M Pokar <129573891+krunal-99@users.noreply.github.com>
2025-05-29 17:28:18 +0530

    Add socket.io integration for real-time messaging and update server setup

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ import cors from "cors";
 dotenv.config();
 import authRoute from "./routes/auth";
 import messageRoute from "./routes/message";
+import { Server } from "socket.io";
+import { createServer } from "http";
 const app = express();
 app.use(cookieParser());
 const PORT = process.env.PORT;
@@ -25,12 +27,36 @@ app.use("/messages", messageRoute);
 AppDataSource.initialize()
   .then(() => {
     console.log("Connected to database successfully.");
-
-    app.listen(PORT, () => {
-      console.log(`Server is running on PORT ${PORT}`);
-    });
   })
   .catch((err) => console.error("Error connecting to database: ", err));
+
+const server = createServer(app);
+const io = new Server(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected to socket.io");
+
+  socket.on("setup", (userData) => {
+    console.log("UserData: ", userData);
+    userData ? socket.join(userData?.id) : "";
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined Room: " + room);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 app.get("/", async (req: Request, res: Response) => {
   res.send("Hello World!");
